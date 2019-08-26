2cceff806b91d3809f4f75aa5c68449e2cf4ad5d
deepakjoseph <deepakjoseph08@gmail.com>
2019-08-26 18:23:14 +0530

    adding a pyscript to jupyter notebook

---
+++ nb_seed.py
@@ -0,0 +1,12 @@
+def function_1():
+	print("you are in function 1")
+	for i in range(10):
+		print(i)
+
+def function_2():
+	print("you are in function 2")
+	for i in range(5):
+		print("*"*i)
+
+
+
